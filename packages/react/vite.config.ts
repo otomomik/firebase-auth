8773b6279f63309430a797c003098c04a5b3b3a8
otomomik <developer@otomomik.com>
2024-04-08 14:02:05 +0900

    chore(react): viteプロジェクトの追加

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: [resolve(__dirname, "src/lib.ts")],
+      name: "FirebaseAuthReact",
+      formats: ["es"],
+      fileName: (format) => `firebase-auth-react.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
