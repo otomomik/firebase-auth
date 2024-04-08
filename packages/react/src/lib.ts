ce3f26a18ed12837ba3223bb18a08fe447485fa9
otomomik <developer@otomomik.com>
2024-04-08 14:07:06 +0900

    chore(react): prettierの追加

--- packages/react/src/lib.ts
+++ packages/react/src/lib.ts
@@ -1,3 +1,3 @@
 export const helloWorld = () => {
-  return "hello world!";
-};
+  return 'hello world!'
+}
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: [resolve(__dirname, "src/lib.ts")],
-      name: "FirebaseAuthReact",
-      formats: ["es"],
+      entry: [resolve(__dirname, 'src/lib.ts')],
+      name: 'FirebaseAuthReact',
+      formats: ['es'],
       fileName: (format) => `firebase-auth-react.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
   },
-});
+})
