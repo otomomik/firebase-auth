9e736dbc7de05622c503b54eb372abf67cb26eb8
otomomik <developer@otomomik.com>
2024-04-19 20:17:51 +0900

    feat(react): devcertの追加

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { UserConfigExport } from 'vite'
 import react from '@vitejs/plugin-react'
+import devcert from 'devcert'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default async (): Promise<UserConfigExport> => {
+  const { key, cert } = await devcert.certificateFor('localhost')
+
+  return {
+    plugins: [react()],
+    server: {
+      https: {
+        key,
+        cert,
+      },
+    },
+  }
+}
